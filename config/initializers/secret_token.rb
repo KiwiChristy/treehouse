d2a473993bcefa592304cea82ab730ed0dcb0aff
Chris <chris@generatormarketing.ie>
2013-01-08 11:49:12 +0000

    Created a rais application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse::Application.config.secret_token = 'b8274565b08c42f0ed876dc0fe6800b37be7a7915e8c3498ac0fe7ba5ef307657cec487a2cf98bcaf4fe59b07d339c0b6f9b6d6566db69253cd1c62f133fcf70'
